b9c6a282fe2c31b5c9d56709d9892fe28a274f14
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-08-25 14:06:07 +0300

    add scripts for games

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+from brain_games.games.brain_even import main
+
+if __name__ == "__main__":
+    if sys.argv[0].endswith("-script.pyw"):
+        sys.argv[0] = sys.argv[0][:-11]
+    elif sys.argv[0].endswith(".exe"):
+        sys.argv[0] = sys.argv[0][:-4]
+    sys.exit(main())
